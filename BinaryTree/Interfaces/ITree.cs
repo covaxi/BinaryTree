c37cb9eb80ec5d4e65460d8fe1e2309a2a6f52ae
Ivan Chernogorsky <covaxi@gmail.com>
2017-08-01 12:01:00 +0300

    Initial commit, solution creation

---
+++ BinaryTree/Interfaces/ITree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Interfaces
+{
+    public interface ITree<T>
+    {
+        /// <summary>
+        /// A value of the node
+        /// </summary>
+        T Value { get; }
+
+        /// <summary>
+        /// Left subtree
+        /// </summary>
+        ITree<T> Left { get; }
+
+        /// <summary>
+        /// Right subtree
+        /// </summary>
+        ITree<T> Right { get; }
+
+        /// <summary>
+        /// Returns true if this node is a leaf
+        /// </summary>
+        bool IsLeaf { get; }
+    }
+}
---
+++ BinaryTreeTests/Nunit.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeTests
+{
+    [TestFixture]
+    public class NunitIntegrationTests
+    {
+        [TestCase]
+        public void NunitInVs()
+        {
+            Assert.True(true, "NUnit is not Working inside Visual Studio");
+        }
+    }
+}
